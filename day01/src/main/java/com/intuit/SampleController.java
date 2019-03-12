c2f1eaca20ea93657f791a0b99ce04840f06debc
prabhu-durasoft <iamprabhu@yahoo.com>
2019-03-12 12:26:45 +0530

    Day01

--- day01/src/main/java/com/intuit/SampleController.java
+++ day01/src/main/java/com/intuit/SampleController.java
@@ -1,11 +1,22 @@
 package com.intuit;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SampleController {
 
+	@PostMapping("/process")
+	public String processName(@RequestParam("firstname") String firstName, HttpSession session) {
+		String message = "Hi " + firstName + "!!!";
+		session.setAttribute("message", message);
+		return "result";
+	}
+	
 	@GetMapping("/")
 	public String index() {
 		return "start";
--- day01/src/main/webapp/start.jsp
+++ day01/src/main/webapp/start.jsp
@@ -1,5 +1,9 @@
 <html>
 	<body>
 		<h1>This is start.jsp</h1>
+		<form action="/process" method="POST">
+			First Name<input type="text" name="firstname"><br/>
+			<button>Click</button>
+		</form>
 	</body>
 </html>
