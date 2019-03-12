fa559ee5a94c701584a7847f58665236d8570e01
prabhu-durasoft <iamprabhu@yahoo.com>
2019-03-12 14:01:37 +0530

    Day01

---
+++ day01jpa/src/main/java/com/intuit/Day01jpaApplication.java
@@ -0,0 +1,13 @@
+package com.intuit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day01jpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day01jpaApplication.class, args);
+	}
+
+}
